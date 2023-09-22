5c7a643b7d4c78647c4afb36eda90327e7d25045
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-22 11:11:25 +0000

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-import { Box, Heading, Select, Spinner, Button, Image, Stack } from "@chakra-ui/react";
+import {
+  chakra,
+  Box,
+  Heading,
+  Select,
+  Spinner,
+  Button,
+  Image,
+  Stack,
+} from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
+import { VscGithubAlt, VscTwitter, VscGlobe } from "react-icons/vsc";
 import kinds from "./kind.json";
 
 async function fetchImage(mode: string): Promise<string> {
@@ -9,7 +19,7 @@ async function fetchImage(mode: string): Promise<string> {
 }
 
 const LoadingSpinner = () => {
-  return <Spinner size="xl" />
+  return <Spinner size="xl" />;
 };
 
 const App = () => {
@@ -41,13 +51,17 @@ const App = () => {
     })();
   }, [setImg, setNowLoading]);
   return (
-    <>
-      <Box w="100%" maxW="xl" mx="auto" px={2}>
-        <Heading textAlign="center" mt="6" mb="2" as="h2" size="2xl">
+    <Box display="flex" flexDirection="column" minHeight="100vh">
+      <Box w="100%" maxW="xl" mx="auto" my="4" px={2}>
+        <Heading textAlign="center" mb="2" as="h2" size="2xl">
           Nekobot Image Viewer
         </Heading>
         <Stack direction={{ base: "column", md: "row" }} spacing={1}>
-          <Select isDisabled={nowLoading} onChange={onChange} placeholder="タイプ選んで">
+          <Select
+            isDisabled={nowLoading}
+            onChange={onChange}
+            placeholder="タイプ選んで"
+          >
             <option value="neko" selected>
               Neko
             </option>
@@ -70,7 +84,41 @@ const App = () => {
         </Stack>
         <Box mt="4">{img}</Box>
       </Box>
-    </>
+      <chakra.footer
+        mt="auto"
+        borderTop="1px"
+        borderColor="gray.400"
+        display="flex"
+        h="20"
+        w="full"
+      >
+        <Stack
+          spacing="20px"
+          direction={["column", "row"]}
+          display="flex"
+          mx="auto"
+          alignItems="center"
+        >
+          <chakra.a
+            display="flex"
+            px="auto"
+            href="https://github.com/tuna2134/neko-api-viewer"
+          >
+            <VscGithubAlt size={35} />
+          </chakra.a>
+          <chakra.a
+            display="flex"
+            px="auto"
+            href="https://twitter.com/fdc_tuna2134"
+          >
+            <VscTwitter size={35} />
+          </chakra.a>
+          <chakra.a display="flex" px="auto" href="https://tuna2134.jp">
+            <VscGlobe size={35} />
+          </chakra.a>
+        </Stack>
+      </chakra.footer>
+    </Box>
   );
 };
 
